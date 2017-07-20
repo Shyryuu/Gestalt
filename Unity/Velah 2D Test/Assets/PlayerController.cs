2cb2945c3e936c65abfbb45f3ad6e7df44f434ae
Xavier Velasco <velasco.coder@gmail.com>
2017-07-20 18:26:57 +0200

    2D Test

---
+++ Unity/Velah 2D Test/Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 5f;
+
+    private Animator anim;
+    private Rigidbody2D rb2d;
+    private Vector2 mov;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+            mov = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        );
+
+        // Update the animation parameters (to update the sprite)
+        if (mov != Vector2.zero)
+        {
+            anim.SetFloat("MovX", mov.x);
+            anim.SetFloat("MovY", mov.y);
+        }
+	}
+
+    // FixedUpdate is called once per frame (alongside physics)
+    void FixedUpdate () {
+        rb2d.MovePosition(rb2d.position + mov * speed * Time.deltaTime);
+    }
+}
