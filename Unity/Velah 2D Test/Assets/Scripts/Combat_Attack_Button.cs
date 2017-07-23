d210f4b4fe82e8c15ca620958d507f4fe12e628e
Xavier Velasco <velasco.coder@gmail.com>
2017-07-23 16:50:09 +0200

    Combat Scene + Transition between Game and Combat Scenes

---
+++ Unity/Velah 2D Test/Assets/Scripts/Combat_Attack_Button.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat_Attack_Button : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Attack(GameObject x, GameObject y)
+    {
+        Debug.Log("<b>Your button works!</b>");
+    }
+}
